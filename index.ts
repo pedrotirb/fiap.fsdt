41789149702086a61bbb0411c71cbab43d7679d0
pedrohbrito2 <pedrohbrito2@gmail.com>
2025-04-10 21:32:04 +0000

    5

---
+++ index.ts
@@ -0,0 +1,10 @@
+  const [likes, setLikes] = useState(0);
+
+  const increaseLikes = () => {
+    setLikes(likes + 1);
+  };
+function useState ( arg0: number ): [ any, any ]
+{
+    throw new Error( "Function not implemented." )
+}
+
